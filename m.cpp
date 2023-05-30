f9b47324ddfc99d081514717efaaf204d5ad1e3f
glucogen19 <98939682+glucogen19@users.noreply.github.com>
2023-05-30 11:05:25 +0400

    5) Создания функции класса

--- m.cpp
+++ m.cpp
@@ -5,5 +5,16 @@ using namespace std;
 class TPen {
 	private:
 	string color;
+	public:
+	string getColor();
+	void setColor(string newColor)
+	{
+    	if (newColor.length() != 6)
+        	color = "FFFFFF";
+    	else if (newColor == "5b92e5") {
+        	cout << "YES!";
+    	}
+    	else color = newColor;
+	}
 };
 int main() {}
